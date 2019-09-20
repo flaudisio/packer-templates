4a925b977077a38f076f31f382444f86c5d5110c
Flaudísio Tolentino <flaudisio@gmail.com>
2019-09-20 15:27:17 -0300

    Add GitLab Runner template

---
+++ templates/_scripts/apt-upgrade.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -eux
+
+sudo apt update
+
+sudo apt upgrade --yes
---
+++ templates/_scripts/wait-cloud-init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -eu
+
+while [ ! -f /var/lib/cloud/instance/boot-finished ] ; do
+    echo "Waiting for cloud-init..."
+    sleep 1
+done
